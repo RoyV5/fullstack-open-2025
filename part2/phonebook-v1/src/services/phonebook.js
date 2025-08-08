a6650f1f6f5a63eeed37db7a1ee41b8a32fe56b6
RoyV5 <rodrigoolmospercoco@gmail.com>
2025-08-08 20:16:32 +0200

    Modified program to work with local backend

--- part2/phonebook-v1/src/services/phonebook.js
+++ part2/phonebook-v1/src/services/phonebook.js
@@ -1,9 +1,11 @@
 import axios from 'axios'
-const baseUrl = "http://localhost:3001/persons"
+const baseUrl = "http://localhost:3001/api/persons"
 
 const getAll = () => {
 	const request = axios.get(baseUrl)
-	return request.then(response => response.data)
+	const response = request.then(response => response.data)
+	console.log(response)
+	return response
 }
 
 const create = newObject => {
